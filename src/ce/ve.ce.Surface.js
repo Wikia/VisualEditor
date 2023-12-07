c5f65c4f83f58bbaf650b0198b7989a1b461e97e
Mateusz Iwaniszczuk <miwaniszczuk@fandom.com>
2023-12-07 17:22:08 +0100

    Revert "UGC-4682 | Fix IME input"
    This reverts commit 65c87d0ab294f7c1c8e70bfbe9e9b6e04b7f6e41.

--- src/ce/ve.ce.Surface.js
+++ src/ce/ve.ce.Surface.js
@@ -3138,12 +3138,7 @@ ve.ce.Surface.prototype.onDocumentBeforeInput = function ( e ) {
 		var surface = this,
 			inputType = e.originalEvent ? e.originalEvent.inputType : null;
 
-		var selectionState = new ve.SelectionState( this.nativeSelection );
-
-		if ( inputType === 'deleteContentBackward' &&
-			selectionState.anchorOffset === 1 &&
-			selectionState.focusOffset === 0
-		) {
+		if ( inputType === 'deleteContentBackward' ) {
 			this.surfaceObserver.pollOnce();
 			ve.ce.keyDownHandlerFactory.lookup( 'linearDelete' ).static.execute( this, e );
 		}
